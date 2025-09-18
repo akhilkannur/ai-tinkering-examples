cb8b99a8776182610122e3abf74a4597bab05c59
akhilkannur <akhil.1000what@gmail.com>
2025-09-18 22:31:10 +0530

    Updated multiple files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-bg': '#f2f7f5',
-        'secondary-bg': '#ffa8ba',
-        'accent': '#faae2b',
-        'text-color': '#00473e',
-        'light-purple': '#475d5b',
-        'bright-pink': '#fa5246',
-        'electric-blue': '#00473e',
-        'navy-dark': '#00332c',
+        'primary-bg': '#fffffe',
+        'secondary-bg': '#ff5470',
+        'accent': '#00ebc7',
+        'text-color': '#00214d',
+        'light-purple': '#1b2d45',
+        'bright-pink': '#fde240',
+        'electric-blue': '#00214d',
+        'navy-dark': '#00214d',
         'navy-light': '#2a3342',
       },
       backgroundImage: {
-        'natsume-gradient': 'linear-gradient(120deg, #faae2b 0%, #fa5246 50%, #ffa8ba 100%)',
+        'natsume-gradient': 'linear-gradient(120deg, #00ebc7 0%, #fde240 50%, #ff5470 100%)',
       }
     },
   },
