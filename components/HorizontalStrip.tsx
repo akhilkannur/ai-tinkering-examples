a210b51cd4cfc67f02cf968dfa864aaedda5ba05
akhilkannur <akhil.1000what@gmail.com>
2025-09-18 13:46:14 +0530

    Updated multiple files

--- components/HorizontalStrip.tsx
+++ components/HorizontalStrip.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Tool } from 'lucide-react'
 
 interface HorizontalStripProps<T> {
   title: string;
