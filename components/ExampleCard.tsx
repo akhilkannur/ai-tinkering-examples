eb07f7cbb8566aa4e77c2dfb22741ac8c738784f
akhilkannur <akhil.1000what@gmail.com>
2025-09-19 10:23:06 +0530

    Updated multiple files

--- components/ExampleCard.tsx
+++ components/ExampleCard.tsx
@@ -97,7 +97,7 @@ export default function ExampleCard({ example, sponsor, priority = false, onOpen
 
             {/* Title Overlay */}
             <div className="absolute bottom-0 left-0 right-0 p-4 bg-[#001858]/80 rounded-b-2xl">
-              <h3 className="text-lg font-semibold leading-tight text-[#fffffe] line-clamp-2">
+              <h3 className="text-lg font-semibold leading-tight text-[#ffffff] line-clamp-2">
                 {example.title}
               </h3>
             </div>
