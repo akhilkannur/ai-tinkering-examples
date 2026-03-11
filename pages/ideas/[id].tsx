b72794c364d50ae79c4534c76cc86ab731ab4834
akhilkannur <akhil.1000what@gmail.com>
2026-03-11 16:58:28 +0530

    fix: restore missing closing div in idea detail page

--- pages/ideas/[id].tsx
+++ pages/ideas/[id].tsx
@@ -108,6 +108,7 @@ export default function IdeaDetailPage() {
                   {idea.whyThisTool}
                 </p>
               </div>
+            </div>
           </div>
         </div>
       </main>
